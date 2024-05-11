9e70cb9d7de9bd822c0e9a47ee3eb35945754060
la-pochoclera <beronalvaro@gmail.com>
2024-05-11 17:03:02 -0300

    Update desafio1_copa_algoritmia.py

--- desafio1_copa_algoritmia.py
+++ desafio1_copa_algoritmia.py
@@ -6,24 +6,24 @@ import random
 # PROGRAMA PRINCIPAL
 contador = 0
 jugadores = [
-    ("Argentina", "Agustina Gorzelany", 11),
-    ("Argentina", "Maria Jose Granatto", 9),
-    ("Argentina", "Sofia Toccalino", 20),
-    ("Argentina", "Agostina Alonso", 10),
-    ("Argentina", "Valentina Raposo", 8),
-    ("Argentina", "Clara Barberi", 5),
-    ("Argentina", "Delfina Thome", 4),
-    ("Argentina", "Sofia Cairo", 7),
-    ("Argentina", "Pilar Campoy", 16),
-    ("Australia", "Alyson Annan", 1),
-    ("Australia", "Katrina Powell", 2),
-    ("Australia", "Stephanie Kershaw", 3),
-    ("Australia", "Jane Claxton", 4),
-    ("Australia", "Claire Colwill", 5),
-    ("Australia", "Rebecca Greiner", 6),
-    ("Australia", "Louise Dobson", 7),
-    ("Australia", "Jodie Schulz", 8),
-    ("Australia", "Mariah Williams", 9)
+    ("Argentina", "Agustina Gorzelany", "11"),
+    ("Argentina", "Maria Jose Granatto", "9"),
+    ("Argentina", "Sofia Toccalino", "20"),
+    ("Argentina", "Agostina Alonso", "10"),
+    ("Argentina", "Valentina Raposo", "8"),
+    ("Argentina", "Clara Barberi", "5"),
+    ("Argentina", "Delfina Thome", "4"),
+    ("Argentina", "Sofia Cairo", "7"),
+    ("Argentina", "Pilar Campoy", "16"),
+    ("Australia", "Alyson Annan", "1"),
+    ("Australia", "Katrina Powell", "2"),
+    ("Australia", "Stephanie Kershaw", "3"),
+    ("Australia", "Jane Claxton", "4"),
+    ("Australia", "Claire Colwill", "5"),
+    ("Australia", "Rebecca Greiner", "6"),
+    ("Australia", "Louise Dobson", "7"),
+    ("Australia", "Jodie Schulz", "8"),
+    ("Australia", "Mariah Williams", "9")
     ]
 
 while contador < 5: 
@@ -32,9 +32,11 @@ while contador < 5:
     print('Jugadora:', jugadora)
 
     pase = random.randint(0, 1)
+    pase = str(pase)
     print('Pase:', pase)
 
     minuto = random.randint(0, 60)
+    minuto = str(minuto)
     print('Minuto:', minuto)
 
     file = open('Pases.txt', 'w')
